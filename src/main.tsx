7f592dfede8d82d00cd5bea5b9ac847ad392a62e
Svitlana <missis.chuvaeva@ukr.net>
2025-06-15 23:43:12 +0300

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './global.css'
+
 import App from './components/App/App'
 import 'modern-normalize/modern-normalize.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
